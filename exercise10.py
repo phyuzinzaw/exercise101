a7099870d0c50d8b34fd641be81e39c9fa46512d
phyuzinzaw <phyuzinzaw644@gmail.com>
2018-06-26 11:38:36 +0630

    changing execrise10 from cat to dog

---
+++ exercise10.py
@@ -0,0 +1,13 @@
+tabby_dog="\tI'm tabbed in."
+persian_dog="I'm split\non aline."
+backslash_dog="I'm\\a\\cat."
+fat_dog="""
+I'll do a list"
+\t* cat food
+\t*fishes
+\t*catnip\n\t*grass
+"""
+print(tabby_dog)
+print(persian_dog)
+print(backslash_dog)
+print(fat_dog)
